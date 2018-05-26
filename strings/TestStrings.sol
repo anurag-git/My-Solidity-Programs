1937a622bf99c4fe65b639594a7628df18f0ffe6
Anurag Dashputre <anurag4u80@gmail.com>
2018-05-26 15:42:30 +0530

    Add files via upload

--- strings/TestStrings.sol
+++ strings/TestStrings.sol
@@ -3,17 +3,17 @@ pragma solidity ^0.4.0;
 import "./StringLib.sol";
 
 contract TestStrings {
-    
+
     using Strings for string;
     string public str1;
-    
+
     constructor (string message) public {
         str1 = message;
     }
     function testConcat(string _base) public {
         str1 = str1.concat(_base);
     }
-    
+
     function testStrcmp(string _base) public view returns (string) {
         uint status = str1.strcmp(_base);
         if(status == 0)
@@ -21,7 +21,7 @@ contract TestStrings {
         else
             return "strings are different";
     }
-    
+
     function testCompareStrings(string str2) public view returns (string) {
         bool status = str1.compareStrings(str2);
         if(status)
